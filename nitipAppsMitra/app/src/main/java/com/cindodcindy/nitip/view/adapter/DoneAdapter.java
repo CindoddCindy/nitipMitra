fd461f34b43fea72748f65cf96bc9a1cd9699b7d
CindoddCindy <cindodcindy@gmail.com>
2021-04-26 20:50:48 +0700

    done item tambah

--- nitipAppsMitra/app/src/main/java/com/cindodcindy/nitip/view/adapter/DoneAdapter.java
+++ nitipAppsMitra/app/src/main/java/com/cindodcindy/nitip/view/adapter/DoneAdapter.java
@@ -2,9 +2,13 @@ package com.cindodcindy.nitip.view.adapter;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.cindodcindy.nitip.R;
 
 public class DoneAdapter extends RecyclerView.Adapter<DoneAdapter.DoneChild> {
 
@@ -26,8 +30,14 @@ public class DoneAdapter extends RecyclerView.Adapter<DoneAdapter.DoneChild> {
 
     public  class  DoneChild extends RecyclerView.ViewHolder{
 
+        private TextView textView_nama_penerima, textView_tggal_diterima;
+        private CardView cardView_done_klik;
+
         public DoneChild(@NonNull View itemView) {
             super(itemView);
+            textView_nama_penerima=itemView.findViewById(R.id.tv_done_item_nama_penerinma);
+            textView_tggal_diterima=itemView.findViewById(R.id.tv_done_item_tggal_diterima);
+            cardView_done_klik=itemView.findViewById(R.id.cv_done);
         }
     }
 }
