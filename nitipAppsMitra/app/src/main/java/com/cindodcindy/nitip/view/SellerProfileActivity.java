3b14ad98cafda4a282e74f410287018b2d5dcbc1
CindoddCindy <cindodcindy@gmail.com>
2021-04-27 09:25:27 +0700

    tambah money list

---
+++ nitipAppsMitra/app/src/main/java/com/cindodcindy/nitip/view/SellerProfileActivity.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.nitip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.nitip.R;
+
+public class SellerProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_profile);
+    }
+}
