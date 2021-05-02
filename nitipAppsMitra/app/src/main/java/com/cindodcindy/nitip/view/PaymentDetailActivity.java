553e571f1bbd171a8114bff8e70e40dba26ce1ef
CindoddCindy <cindodcindy@gmail.com>
2021-05-02 17:48:01 +0700

    tambah detail payments

--- nitipAppsMitra/app/src/main/java/com/cindodcindy/nitip/view/PaymentDetailActivity.java
+++ nitipAppsMitra/app/src/main/java/com/cindodcindy/nitip/view/PaymentDetailActivity.java
@@ -56,6 +56,42 @@ public class PaymentDetailActivity extends AppCompatActivity {
 
     public  void getDataDetailPayment(){
 
+        if(getIntent().getExtras()!=null) {
+            /**
+             * Jika Bundle ada, ambil data dari Bundle
+             */
+            Bundle bundle = getIntent().getExtras();
+
+            // spHandle.setSpIdConfirmOrderEdit(SpHandle.SP_ID_CONFIRM_ORDER_EDIT, bundle.getLong("id_customer"));
+            //spHandle.setSpIdConfirmOrder(SpHandle.SP_ID_CONFIRM_ORDER,bundle.getLong("id_confirm"));
+            textView_asal.setText(bundle.getString("asal"));
+            textView_tujuan.setText(bundle.getString("tujuan"));
+            textView_date_going.setText(bundle.getString("tglgo"));
+            textView_date_arive.setText(bundle.getString("tglarr"));
+            textView_time_going.setText(bundle.getString("jamgo"));
+            textView_time_arrive.setText(bundle.getString("jamarr"));
+            textView_nama_penjual.setText(bundle.getString("namapenjual"));
+            textView_harga.setText(bundle.getString("harga"));
+            textView_kapasitas.setText(bundle.getString("kapasitas"));
+            textView_barang_type.setText(bundle.getString("jenisbarang"));
+
+            textView_asal_barang.setText(bundle.getString("asalBr"));
+            textView_tujuan_barang.setText(bundle.getString("tujuanBr"));
+            textView_nama_pengirim.setText(bundle.getString("pengirim"));
+            textView_nama_penerima.setText(bundle.getString("penerima"));
+            textView_jenis_barang.setText(bundle.getString("jenisBr"));
+            textView_berat_kg.setText(bundle.getString("beratBr"));
+
+            textView_uang_nama_tf.setText(bundle.getString("namaPengirimUang"));
+            textView_uang_tgal_tf.setText(bundle.getString("TanggalUangDikirim"));
+            textView_uang_jumlah_tf.setText(bundle.getString("JumlahUangDikirim"));
+            textView_uang_bank_asal.setText(bundle.getString("BankPengirim"));
+
+
+
+
+        }
+
     }
 
     public void hapusDataPayment(){
--- nitipAppsMitra/app/src/main/java/com/cindodcindy/nitip/view/adapter/PaymentAdapter.java
+++ nitipAppsMitra/app/src/main/java/com/cindodcindy/nitip/view/adapter/PaymentAdapter.java
@@ -1,5 +1,9 @@
 package com.cindodcindy.nitip.view.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,17 +13,79 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cindodcindy.nitip.R;
+import com.cindodcindy.nitip.pojo.pojo_payment.get_payement.Content;
+import com.cindodcindy.nitip.view.BookingDetailActivity;
+import com.cindodcindy.nitip.view.PaymentDetailActivity;
+
+import java.util.List;
 
 public class PaymentAdapter extends RecyclerView.Adapter<PaymentAdapter.PaymentChild> {
+
+    public List<com.cindodcindy.nitip.pojo.pojo_payment.get_payement.Content> contentList;
+    public Context context;
+
+    public  PaymentAdapter(List<com.cindodcindy.nitip.pojo.pojo_payment.get_payement.Content> contentList, Context context){
+        this.contentList=contentList;
+        this.context=context;
+
+    }
+
 
     @NonNull
     @Override
     public PaymentChild onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.booking_item_layout, parent, false);
+        PaymentChild mViewHolder = new PaymentChild(mView);
+
+        return mViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull PaymentChild holder, int position) {
+
+        final Content content= contentList.get(position);
+        holder.textView_nama_pengirim.setText(content.getNamaPengirimUang());
+        holder.textView_tgal_kirim.setText(content.getTanggalTransfer());
+        holder.textView_jumlah_uang.setText(content.getUangSejumlah());
+        holder.textView_nama_bank.setText(content.getBankPengirimUang());
+
+        holder.cardView_payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putLong("id_buyer", content.getIdBuyer());
+                bundle.putString("asal",content.getAsal());
+                bundle.putString("tujuan",content.getTujuan());
+                bundle.putString("tglgo",content.getTanggalBerangkat());
+                bundle.putString("tglarr",content.getTanggalTiba());
+                bundle.putString("jamgo", content.getJamBerangkat());
+                bundle.putString("jamarr",content.getJamTiba());
+                bundle.putString("namapenjual",content.getNamaPenjual());
+                bundle.putString("kapasitas",content.getKapasitas());
+                bundle.putString("jenisbarang",content.getJenisBarang());
+                bundle.putString("harga", content.getHargaBagasi());
+
+                bundle.putString("asalBr",content.getAlamatPembeli());
+                bundle.putString("tujuanBr",content.getAlamatPenerima());
+                bundle.putString("pengirim",content.getNamaPembeli());
+                bundle.putString("penerima",content.getNamaPenerima());
+                bundle.putString("jenisBr", content.getJenisBarangKirim());
+                bundle.putString("beratBr",content.getKapasitasBarang());
+
+                bundle.putString("namaPengirimUang",content.getNamaPengirimUang());
+                bundle.putString("TanggalUangDikirim",content.getTanggalTransfer());
+                bundle.putString("JumlahUangDikirim",content.getUangSejumlah());
+                bundle.putString("BankPengirim",content.getBankPengirimUang());
+
+
+                Intent intent = new Intent(context, PaymentDetailActivity.class);
+                intent.putExtras(bundle);
+                context.startActivity(intent);
+
+            }
+        });
 
     }
 
