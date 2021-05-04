5b5d8af42fd01b5654b730206fe5ec43e3a6207d
CindoddCindy <cindodcindy@gmail.com>
2021-05-04 14:34:19 +0700

    tambah adapter money

--- nitipAppsMitra/app/src/main/java/com/cindodcindy/nitip/view/adapter/MoneyAdapter.java
+++ nitipAppsMitra/app/src/main/java/com/cindodcindy/nitip/view/adapter/MoneyAdapter.java
@@ -1,5 +1,9 @@
 package com.cindodcindy.nitip.view.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,17 +13,79 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cindodcindy.nitip.R;
+import com.cindodcindy.nitip.pojo.pojo_money.pojo_get_money.Content;
+import com.cindodcindy.nitip.view.GetMoneyDetailActivity;
+
+import java.util.List;
 
 public class MoneyAdapter extends RecyclerView.Adapter<MoneyAdapter.MoneyChild> {
+
+
+    public List<Content> contentList;
+    public Context context;
+
+    public  MoneyAdapter(List<Content> contentList, Context context){
+        this.contentList=contentList;
+        this.context=context;
+
+    }
 
     @NonNull
     @Override
     public MoneyChild onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.money_item_layout, parent, false);
+        MoneyChild mViewHolder = new MoneyChild(mView);
+        return mViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MoneyChild holder, int position) {
+        final Content content= contentList.get(position);
+        holder.textView_nama_pengirim.setText(content.getAnPengirim());
+        holder.textView_jumlah_uang.setText(content.getJumlahUang());
+        holder.textView_nama_bank.setText(content.getDariBank());
+        holder.textView_tgal_dikirm.setText(content.getTanggalTransfer());
+
+        holder.cardView_moneys.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putLong("id_seller", content.getIdSeller());
+                bundle.putString("asal",content.getAsal());
+                bundle.putString("tujuan",content.getTujuan());
+                bundle.putString("tglgo",content.getTanggalBerangkat());
+                bundle.putString("tglarr",content.getTanggalTiba());
+                bundle.putString("jamgo", content.getJamBerangkat());
+                bundle.putString("jamarr",content.getJamTiba());
+                bundle.putString("namapenjual",content.getNamaPenjual());
+                bundle.putString("kapasitas",content.getKapasitas());
+                bundle.putString("jenisbarang",content.getJenisBarang());
+                bundle.putString("harga", content.getHargaBagasi());
+
+                bundle.putString("asalBr",content.getAlamatPembeli());
+                bundle.putString("tujuanBr",content.getAlamatPenerima());
+                bundle.putString("pengirim",content.getNamaPembeli());
+                bundle.putString("penerima",content.getNamaPenerima());
+                bundle.putString("jenisBr", content.getJenisBarangKirim());
+                bundle.putString("beratBr",content.getKapasitasBarang());
+
+                bundle.putString("namaPenerimaUang",content.getAnPengirim());
+                bundle.putString("jumlahUang",content.getJumlahUang());
+                bundle.putString("bankTf",content.getDariBank());
+                bundle.putString("tanggalTf",content.getTanggalTransfer());
+
+
+                bundle.putString("tanggalBarangTiba",content.getTglBrgArr());
+                bundle.putString("namaPenerimaBarang",content.getAnBrgArr());
+                bundle.putString("lokasiBarangDiterima",content.getLocBrgArr());
+
+                Intent intent = new Intent(context, GetMoneyDetailActivity.class);
+                intent.putExtras(bundle);
+                context.startActivity(intent);
+
+            }
+        });
 
     }
 
